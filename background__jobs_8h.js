6154c38cc11dced276eb30efcf6444df931778e3
marcosfons <marcosfons@users.noreply.github.com>
2022-05-17 22:38:01 +0000

    deploy: bb867315fe0a7fdff3e4dd3bbb9a72fb83f43279

--- background__jobs_8h.js
+++ background__jobs_8h.js
@@ -7,6 +7,7 @@ var background__jobs_8h =
     [ "add_background_job", "background__jobs_8h.html#a6949e27ddbcafe827c27444388e907bb", null ],
     [ "create_background_jobs", "background__jobs_8h.html#a4597adb9fe554e7b314c966736b50758", null ],
     [ "free_background_job", "background__jobs_8h.html#a74a9ff948dff90a1a31d9acc7a098d50", null ],
+    [ "free_background_job_chain", "background__jobs_8h.html#a5be1c3903a84c9a6547398acf941668b", null ],
     [ "free_background_jobs", "background__jobs_8h.html#a7816e75150f7dca33a4d3b46ecdd9efd", null ],
     [ "remove_command_by_pid", "background__jobs_8h.html#a43584b96a56b8aa5e737f812e2972611", null ],
     [ "update_background_job_status", "background__jobs_8h.html#ae6e57676cb897218633deed50a5cf81b", null ],
